cdcfe4d74e4519fa0eb8ef43c55c415ad4a566e9
kaizen-tech <kaizen.techplus@gmail.com>
2022-07-31 12:15:32 +0530

    added drop down

--- app/src/main/java/com/hipla/channel/ui/ApplicationCustomerInfoFragment.kt
+++ app/src/main/java/com/hipla/channel/ui/ApplicationCustomerInfoFragment.kt
@@ -1,33 +1,42 @@
 package com.hipla.channel.ui
 
 import android.os.Bundle
-import android.util.Log
+import android.view.KeyEvent.ACTION_UP
 import android.view.View
-import android.widget.Toast
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.hipla.channel.R
-import com.hipla.channel.common.LogConstant
-import com.hipla.channel.common.Utils
-import com.hipla.channel.databinding.FragmentApplicationBinding
 import com.hipla.channel.databinding.FragmentApplicationCustomerInfoBinding
-import com.hipla.channel.screenHeightInDp
-import com.hipla.channel.screenWidthDp
-import com.hipla.channel.ui.adapter.SalesRecyclerAdapter
-import com.hipla.channel.ui.decoration.SalesGridItemDecoration
 import com.hipla.channel.viewmodel.MainViewModel
+
 
 class ApplicationCustomerInfoFragment : Fragment(R.layout.fragment_application_customer_info) {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: FragmentApplicationCustomerInfoBinding
+    private var floorList =
+        arrayOf("First floor", "Second floor", "Third floor", "Fourth floor", "First floor", "Second floor", "Third floor", "Fourth floor")
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentApplicationCustomerInfoBinding.bind(view)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        setFloorPreference()
+    }
+
+    private fun setFloorPreference() {
+        val floorAdapter: ArrayAdapter<String> =
+            ArrayAdapter(requireContext(), R.layout.autocomplete_list_item, floorList)
+        binding.floorPreference.run {
+            setAdapter(floorAdapter)
+            setOnTouchListener { _, motionEvent ->
+                if (motionEvent.action == ACTION_UP) {
+                    showDropDown()
+                }
+                return@setOnTouchListener false;
+            }
+        }
     }
 
 }
